b1b2d4a6a06033dbb29a6a9bce9794ec167300eb
dragon3h <dragon3h@gmx.com>
2024-04-05 07:02:01 -0400

    initial commit

---
+++ src/main/java/com/romr/whitehall/WhitehallApplication.java
@@ -0,0 +1,13 @@
+package com.romr.whitehall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitehallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitehallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romr/whitehall/WhitehallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romr.whitehall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitehallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
